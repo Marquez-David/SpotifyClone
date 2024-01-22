9d9df690877589f267e544062533df2c27bbb4b1
David Márquez Mínguez <davidmarquezminguez@gmail.com>
2024-01-22 10:54:07 +0100

    Update next.config.js

--- web/next.config.js
+++ web/next.config.js
@@ -3,8 +3,19 @@ const nextConfig = {
     CLIENT_SECRET: process.env.CLIENT_SECRET,
   },
   images: {
-    domains: ['mosaic.scdn.co', 'i.scdn.co'],
-  },
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'mosaic.scdn.co',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.scdn.co',
+        pathname: '**',
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
