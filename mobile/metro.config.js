9a7f72bd3eb24b196eb42e76804f55bf3d0c7686
David Márquez Mínguez <45391647+Marquez-David@users.noreply.github.com>
2024-01-23 10:30:55 +0100

    Update metro.config.js

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -2,8 +2,6 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 
 /**
  * Metro configuration
- * https://facebook.github.io/metro/docs/configuration
- *
  * @type {import('metro-config').MetroConfig}
  */
 const config = {};
